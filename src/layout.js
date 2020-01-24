c95eeac67409ee88bcf1d2e87202e9c8c937e6c4
jassan leyva <jassan851031@gmail.com>
2020-01-24 14:42:02 -0300

    added views jorge

--- src/layout.js
+++ src/layout.js
@@ -11,17 +11,14 @@ import Home_admin from './views/home_admin';
 import Mensajeria from './components/modal_mensajeria';
 import Ventas_garzon from './views/ventas_garzon';
 import Add_item from './views/add_item';
-<<<<<<< HEAD
 import Add_sub_categoria from './views/add_sub_categoria';
 import Productos from './views/productos_admin';
 import Modificar_item from './views/modificar_item';
 import Juntar_mesa from './components/modal_juntar_mesa';
-=======
 import Horario from './views/horario';
 import Menu from './views/menu';
 import NuevoUsuario from './views/nuevoUsuario';
 import Register from './views/register';
->>>>>>> eb598a542c9c50727151094b81867f7573252aa4
 
 const Layout = props => {
     return (
@@ -31,27 +28,20 @@ const Layout = props => {
                 <Route exact path="/" component={Login} />
                 <Route exact path="/home/modal_ingredientes" component={Modal_ingredientes} />
                 <Route exact path="/home/modal_menu" component={Modal_menu} />
-<<<<<<< HEAD
                 <Route exact path="/productos/modificar_item" component={Modificar_item} />
-                <Route exact path="/login" component={Login} />
-=======
->>>>>>> eb598a542c9c50727151094b81867f7573252aa4
                 <Route exact path="/home" component={Home} />
                 <Route exact path="/dashboard" component={Home_admin} />
                 <Route exact path="/mensajeria" component={Mensajeria} />
                 <Route exact path="/ventas_garzon" component={Ventas_garzon} />
                 <Route exact path="/add_items" component={Add_item} />
-<<<<<<< HEAD
                 <Route exact path="/add_sub_categoria" component={Add_sub_categoria} />
                 <Route exact path="/productos" component={Productos} />
                 <Route exact path="/juntar" component={Juntar_mesa} />
-=======
                 <Route exact path ="/horario" component={Horario}/>
                 <Route exact path ="/menu" component={Menu}/>
                 <Route exact path= "/nuevoUsuario" component={NuevoUsuario}/>
                 <Route exact path= "/register" component={Register}/>
 
->>>>>>> eb598a542c9c50727151094b81867f7573252aa4
                 {/* <Route render={() => <h1>Not Found</h1>} /> */}
             </Switch>
             <Footer />
