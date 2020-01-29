1b8435de1804c0815ba6c75492058c5d34f801b9
jipizarroo <jipizarroo@gmail.com>
2020-01-29 11:50:15 -0300

    Register y test x error hoooks-Javier

--- src/layout.js
+++ src/layout.js
@@ -20,7 +20,7 @@ import Juntar_mesa from './components/modal_juntar_mesa';
 import Horario from './views/horario';
 import Menu from './views/menu';
 import NuevoUsuario from './views/nuevoUsuario';
-import Register from './views/register';
+import Register from './components/register';
 import Admi_Usuario from './views/admi_Usuario';
 import Modal_add_items from './components/modal_add_items'
 
@@ -31,16 +31,17 @@ const Layout = props => {
             <Navbar />
             <Switch>
                 <Route exact path="/" component={Login} />
+                <Route exact path="/admin_dashboard" component={Home_admin} />
+                <Route exact path ="/admin_dashboard/users" component={Admi_Usuario}/>
+                <Route exact path= "/admin_dashboard/users/register" component={Register}/>
                 <Route exact path="/home/modal_ingredientes" component={Modal_ingredientes} />
                 <Route exact path="/home/modal_menu" component={Modal_menu} />
                 <Route exact path="/modalConfiguracion" component={ModalConfiguracion}/>
                 <Route exact path ="/modalEliminar" component={ModalEliminar}/>
-                <Route exact path ="/admi_Usuario" component={Admi_Usuario}/>
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/home/modal_add_items" component={Modal_add_items} />
                 <Route exact path="/productos/modificar_item" component={Modificar_item} />
                 <Route exact path="/home" component={Home} />
-                <Route exact path="/dashboard" component={Home_admin} />
                 <Route exact path="/mensajeria" component={Mensajeria} />
                 <Route exact path="/ventas_garzon" component={Ventas_garzon} />
                 <Route exact path="/add_items" component={Add_item} />
@@ -50,7 +51,6 @@ const Layout = props => {
                 <Route exact path ="/horario" component={Horario}/>
                 <Route exact path ="/menu" component={Menu}/>
                 <Route exact path= "/nuevoUsuario" component={NuevoUsuario}/>
-                <Route exact path= "/register" component={Register}/>
 
                 {/* <Route render={() => <h1>Not Found</h1>} /> */}
             </Switch>
